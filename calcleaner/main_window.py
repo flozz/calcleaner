db4c469a170b1250da6ab25d8b09e856f303dab9
Fabien LOISON <flo@flogisoft.com>
2022-06-29 14:04:42 +0200

    WIP: Main window

--- calcleaner/main_window.py
+++ calcleaner/main_window.py
@@ -1,6 +1,7 @@
 from gi.repository import Gtk
 
 from . import APPLICATION_NAME
+from . import data_helpers
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -10,6 +11,13 @@ class MainWindow(Gtk.ApplicationWindow):
             application=app,
             title=APPLICATION_NAME,
             default_width=400,
-            default_height=500,
+            default_height=300,
             resizable=True,
         )
+
+        self._builder = Gtk.Builder()
+        self._builder.add_from_file(data_helpers.find_data_path("ui/main-window.glade"))
+        self._builder.connect_signals(self)
+
+        content = self._builder.get_object("main-window-content")
+        self.add(content)
