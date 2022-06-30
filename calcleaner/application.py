0e7d1e217c4373fd54d2aef3eda3e9e74d9e052f
Fabien LOISON <flo@flogisoft.com>
2022-06-30 13:50:30 +0200

    Main Windows / Application: implements main workflow

--- calcleaner/application.py
+++ calcleaner/application.py
@@ -3,9 +3,13 @@ from gi.repository import Gio
 
 from . import APPLICATION_ID
 from .main_window import MainWindow
+from .caldav_dialog import CaldavDialog
 
 
 class CalcleanerApplication(Gtk.Application):
+
+    accounts = {}
+
     def __init__(self):
         Gtk.Application.__init__(
             self,
@@ -17,8 +21,40 @@ class CalcleanerApplication(Gtk.Application):
     def do_startup(self):
         Gtk.Application.do_startup(self)
 
+        # Action: app.add-caldav
+        action = Gio.SimpleAction.new("add-caldav", None)
+        action.connect("activate", lambda a, p: self.add_caldav())
+        self.add_action(action)
+
     def do_activate(self):
         if not self._main_window:
             self._main_window = MainWindow(self)
+
         self._main_window.show()
         self._main_window.present()
+
+    def add_caldav(self):
+        caldav_dialog = CaldavDialog(parent_window=self._main_window)
+
+        # XXX DEBUG ===============================================================
+        caldav_dialog._url_entry.set_text("http://localhost:8080/remote.php/dav")
+        caldav_dialog._username_entry.set_text("admin")
+        caldav_dialog._password_entry.set_text("password")
+        # XXX DEBUG ===============================================================
+
+        caldav_account = caldav_dialog.run()
+
+        if caldav_account:
+            self.accounts[caldav_account["url"]] = {
+                "username": caldav_account["username"],
+                "password": caldav_account["password"],
+                "calendars": {
+                    # "url": {"name": name, "color": color, "events_count": 0}
+                },
+            }
+            self.fetch_calendars()
+
+    def fetch_calendars(self):
+        self._main_window.set_state(self._main_window.STATE_UPDATING)
+        # TODO
+        self._main_window.set_state(self._main_window.STATE_CALENDAR_LIST)
--- calcleaner/main_window.py
+++ calcleaner/main_window.py
@@ -5,6 +5,11 @@ from . import data_helpers
 
 
 class MainWindow(Gtk.ApplicationWindow):
+
+    STATE_INITIAL = "state-initial"
+    STATE_UPDATING = "state-updating"
+    STATE_CALENDAR_LIST = "state-calendar-list"
+
     def __init__(self, app):
         Gtk.ApplicationWindow.__init__(
             self,
@@ -21,3 +26,14 @@ class MainWindow(Gtk.ApplicationWindow):
 
         content = self._builder.get_object("main-window-content")
         self.add(content)
+
+        self.set_state(self.STATE_INITIAL)
+
+    def set_state(self, state):
+        initial_root = self._builder.get_object("state-initial")
+        updating_root = self._builder.get_object("state-updating")
+        calendar_list_root = self._builder.get_object("state-calendar-list")
+
+        initial_root.set_visible(state == self.STATE_INITIAL)
+        updating_root.set_visible(state == self.STATE_UPDATING)
+        calendar_list_root.set_visible(state == self.STATE_CALENDAR_LIST)
