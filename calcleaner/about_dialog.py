f21dcb3c96ed42ab545838f2dd9149e3d7035b0a
Fabien LOISON <flo@flogisoft.com>
2022-07-11 18:06:17 +0200

    Add about dialog

---
+++ calcleaner/about_dialog.py
@@ -0,0 +1,26 @@
+from gi.repository import Gtk
+from gi.repository import GdkPixbuf
+
+from . import APPLICATION_NAME
+from . import VERSION
+from . import data_helpers
+
+
+class AboutDialog(Gtk.AboutDialog):
+    def __init__(self, parent=None):
+        Gtk.AboutDialog.__init__(
+            self,
+            parent=parent,
+            program_name=APPLICATION_NAME,
+            comments="A simple graphical tool to purge old events from CalDAV calendars",
+            version=VERSION,
+            copyright="Copyright (c) 2022 Fabien LOISON",
+            website_label="github.com/flozz/calcleaner",
+            website="https://github.com/flozz/calcleaner",
+            license_type=Gtk.License.GPL_3_0,
+        )
+
+        logo = GdkPixbuf.Pixbuf.new_from_file(
+            data_helpers.find_data_path("images/calcleaner_128.png")
+        )
+        self.set_logo(logo)
